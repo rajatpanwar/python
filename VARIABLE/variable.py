6988e7e26de06ab664a9d7a620fbf1b927dda79a
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-13 12:48:54 +0530

    basics os variable

---
+++ VARIABLE/variable.py
@@ -0,0 +1,30 @@
+x,y,z= "rajat","rithik","nipun"
+print(x)
+print(y)
+print(z)         //output is rajat , rithik ,nipun
+
+
+
+
+<-----2nd example---->
+
+x = y = z = "Rajat"
+print(x)
+print(y)
+print(z)     //output is -- Rajat,Rajat, Rajat
+
+
+
+<---3rd example--->
+ 
+ --add two string--
+x = "awesome"
+print("Python is " + x)    //output is -- python is awesome
+
+<---4th example--->
+
+//we can write 3rd example with the help of this 3rd variable
+x="Awesome"
+y="python is"
+z=(y+x)
+print(z)        //output is--- python is awesome
