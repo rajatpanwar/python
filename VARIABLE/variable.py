68b2d583de3ae5b98aa88702e77ba64f31b7bc09
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-13 13:00:05 +0530

    global variable

--- VARIABLE/variable.py
+++ VARIABLE/variable.py
@@ -28,3 +28,14 @@ x="Awesome"
 y="python is"
 z=(y+x)
 print(z)        //output is--- python is awesome
+
+
+
+<----GLOBAL VARIABLE---->
+
+variable that are created outside the function called gloabal variable
+
+x="Awesome"
+  def fun()                //def keyword is used in python to define the function and we can write def before the function name
+ print("python is "+x)
+ fun()                   //output is --pyton is Awesome
