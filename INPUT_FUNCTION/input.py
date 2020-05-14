efdf503e883332a4737ba7186b8b80127d4a1e0b
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-14 11:42:50 +0530

    basic example of input function

--- INPUT_FUNCTION/input.py
+++ INPUT_FUNCTION/input.py
@@ -4,3 +4,15 @@
 name = input("enter the name")
 print("hello"+name)
 ///output is---- hello rajat 
+
+
+
+
+<<<------example of input------->
+name = input("enter the name")
+print("hello"+name)
+
+age = input("enter the age")
+print("age is"+age)
+
+
