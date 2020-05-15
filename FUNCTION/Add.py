831e23c8561e2a9b835d9cb4528e86277c2ea548
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-15 13:50:08 +0530

    now this is the right way to add two number

--- FUNCTION/Add.py
+++ FUNCTION/Add.py
@@ -7,5 +7,12 @@ n2=input("enter the second no")
 ttal=n1+n2
 
 print(ttal)    //output is ---44 
-
 //bcoz of input function takke input from user as a string  
+
+<------example2----->
+n1=int(input("enter the first no"))
+n2=int(input("enter the second no"))
+ttal=n1+n2
+print(str(ttal))    //output is--8
+
+
