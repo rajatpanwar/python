4a4ea96717084d21106f52dc1e1041fdc27ccbef
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-15 14:50:47 +0530

    using only if condition to check which number is largsest

---
+++ IF-ELSE/IF.py
@@ -0,0 +1,11 @@
+n1=int(input("enter the first no"))
+n2=int(input("enter the second no"))
+
+n3=int(input("enter the third no"))
+
+if n1>n2 and n1>n3:
+   print("largest no "+str(n1))
+if n2>n1 and n2>n3:
+   print("largest no "+str(n2))
+if n3>n1 and n3>n2:
+   print("largest no "+str(n3))
