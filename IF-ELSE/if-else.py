5920564fc9098f43887f46bf3ebd697cc62a2ab9
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-15 14:59:16 +0530

    use elif condition

---
+++ IF-ELSE/if-else.py
@@ -0,0 +1,23 @@
+<-----example1------->
+
+age=int(input("enter the age"))
+
+if age>=18:
+    print("elegible for vote")
+else:
+    print("not elegible")
+
+
+
+<-------example2-------->
+//use elif condition
+
+number = int(input("Enter the number?"))  
+if number==10:  
+    print("number is equals to 10")  
+elif number==50:  
+    print("number is equal to 50");  
+elif number==100:  
+    print("number is equal to 100");  
+else:  
+    print("number is not equal to 10, 50 or 100");  
