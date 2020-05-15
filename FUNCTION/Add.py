97cdcab87d54d2b6ce3c8f8c65d6fbf7ac571977
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-15 13:45:05 +0530

    function

---
+++ FUNCTION/Add.py
@@ -0,0 +1,11 @@
+in theses exapmle we ll discuss about function
+
+<------example1------->
+
+n1=input("enter the first no")
+n2=input("enter the second no")
+ttal=n1+n2
+
+print(ttal)    //output is ---44 
+
+//bcoz of input function takke input from user as a string  
