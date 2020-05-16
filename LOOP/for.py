800efb6eb8621eda18d0ed9b005d1d0190738bb1
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-16 15:26:39 +0530

    output like this

--- LOOP/for.py
+++ LOOP/for.py
@@ -2,4 +2,13 @@ i=1;
 n1=int(input("enter the number - "))
 for i in range(1,11):
     print("%dx%d=%d"%(n1,i,n1*i))
+    
+    
+    //output is 
+    10x1=10
+    10x2=20
+    .
+    .
+    .
+    10x10=100
 
