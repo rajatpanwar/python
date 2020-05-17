793f5977273138807d602d8b8a4029f1c85a7588
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-17 13:46:59 +0530

    input from user in second example

--- LIST/loop_list/loop.py
+++ LIST/loop_list/loop.py
@@ -1,3 +1,13 @@
 emp=["rajat",22,500069414]
 for i in emp:
 print(i)
+
+
+,<--------another example------->
+ist=[]
+no=int(input("enter the no of elements in the list: "))
+for i in range(0,no):
+    list.append(input("enter the item  "))
+print("print the list item ")
+for i in list:
+    print(i,end=" ")
