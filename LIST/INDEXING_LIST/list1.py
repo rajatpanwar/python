2b09de05bc3b3aaf6864e497b2d09870f22ce5a4
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-17 13:24:38 +0530

    another example

--- LIST/INDEXING_LIST/list1.py
+++ LIST/INDEXING_LIST/list1.py
@@ -1,2 +1,8 @@
 fruits = ["apple", "banana", "cherry"]
 print(fruits[-1])                       //Negative indexing means beginning from the end, -1 refers to the last item
+
+
+<-----example2----->
+fruits1 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits1[2:5])   //Return the third, fourth, and fifth item:
+  
