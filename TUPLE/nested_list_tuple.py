6f0ea2da2e6be21d754c16ddc2813109fd9dfab5
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-18 16:42:19 +0530

    nested list and tupple

---
+++ TUPLE/nested_list_tuple.py
@@ -0,0 +1,11 @@
+employee=[(80, "rajat", 414),(84, "rithik", 261),(82, "nipun", 9052),(83, "harshil", 369)]
+
+print("print the employee list----------->")
+for i in employee:
+    print(i)
+print()
+
+employee[0]=(82,"rakshit",463)
+print("after modification---------->")
+for i in employee:
+    print(i)
