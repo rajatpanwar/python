40efad2db378f818ee62fa1524978e111a1c7d45
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-18 18:39:00 +0530

    second example

--- SET/Set.py
+++ SET/Set.py
@@ -2,3 +2,13 @@
 
 set = {"apple", "banana", "cherry"}
 print(set)
+
+
+<--------example2----------->
+Days={"Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday"}
+print(Days)
+print(type(Days))
+print("------through looping print the days-----------")
+for i in Days:
+    print(i)
+    
