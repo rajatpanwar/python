c939f4be2360268689f686028ca94a031c264418
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-18 19:08:26 +0530

    using update() function

---
+++ SET/Update.py
@@ -0,0 +1,9 @@
+//update() function use the update for list
+
+
+Days={"Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday"}
+print(Days)
+
+Days.update(["januarry","February"])
+print("----modified list------>")
+print(Days)
