46cc301c6ab7091b5adfbb2ea6a1c7a990867895
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-18 15:58:29 +0530

    another example

--- TUPLE/Tuple.py
+++ TUPLE/Tuple.py
@@ -3,3 +3,12 @@ print(tuple)
 count=0;
 for i in tuple:
     print("tupple[%d]=%d"%(count,i))
+    
+    
+  <------------------example2------------------>
+no=tuple(input("enter the element"))    //take input as a string from user
+print(no)
+count=0;
+for i in no:
+    print("tupple[%d]=%s"%(count,i))
+
