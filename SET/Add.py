ebd4209810f9ececd99125817f95238ba369542a
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-18 18:59:13 +0530

    using add() function

---
+++ SET/Add.py
@@ -0,0 +1,7 @@
+////using add() function we can add element in the set
+
+Days={"Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday"}
+print(Days)
+
+Days.add("orange")
+print(Days)
