948dd2b8a23a020a4fc2b8b906b4e00c5b934d98
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-21 09:24:31 +0530

    basic example of dictionary

---
+++ Dictonary/Dictionary.py
@@ -0,0 +1,9 @@
+/// A dictionary is a collection which is unordered, changeable and indexed. In Python dictionaries are written with curly brackets
+
+<--------example1------->
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
