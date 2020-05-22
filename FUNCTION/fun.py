e23f396ccaf5640af28b55c0da8434837eadbb21
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-22 10:56:27 +0530

    function's defination and advantage

---
+++ FUNCTION/fun.py
@@ -0,0 +1,7 @@
+/// A function can be defined as the organized block of reusable code which can be called whenever required. Python allows us to divide a large program into the basic building blocks known as function. The function contains the set of programming statements enclosed by {}.
+
+<----------------------ADVANTAGE----------------------------->
+AVOID REWRITING
+REUSABLITY
+WE CAN TRACK A LARGE PYTHON PROGRAM EASILY WHEN IT IS DEVIDED INTO MULTIPLE FUNCTION
+
