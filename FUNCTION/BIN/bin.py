6d5b18c0d83bcb994ea67f64fd377fe0ccce1e5d
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-23 23:07:14 +0530

    and the small program of inbuilt function

--- FUNCTION/BIN/bin.py
+++ FUNCTION/BIN/bin.py
@@ -1,1 +1,6 @@
 ////bin() function is used to return the binary representation of a specified integer. A result always starts with the prefix 0b.
+
+
+x =  10  
+y =  bin(x)  
+print (y)  ///output is  ----0b1010
