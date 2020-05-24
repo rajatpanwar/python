97bc2e582a0df63f60831613ca2a4e71b2fbd1d1
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-24 22:22:45 +0530

    after syntax

--- FILE HANDLING/file.py
+++ FILE HANDLING/file.py
@@ -1,5 +1,9 @@
 // Till now, we were taking the input from the console and writing it back to the console to interact with the user.
 
-Sometimes, it is not enough to only display the data on the console. The data to be displayed may be very large, and only a limited amount of data can be displayed on the console, and since the memory is volatile, it is impossible to recover the programmatically generated data again and again.
+//Sometimes, it is not enough to only display the data on the console. The data to be displayed may be very large, and only a limited amount of data can be displayed on the console, and since the memory is volatile, it is impossible to recover the programmatically generated data again and again.
 
-However, if we need to do so, we may store it onto the local file system which is volatile and can be accessed every time. Here, comes the need of file handling.
+//However, if we need to do so, we may store it onto the local file system which is volatile and can be accessed every time. Here, comes the need of file handling.
+
+
+
+file object = open(<file-name>, <access-mode>, <buffering>)  //the syntax to use the open() function is
