0e6068b8789b7e84552a4a035744e2cbcf173f8d
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-24 21:38:58 +0530

    defination of lembda function

---
+++ FUNCTION/LAMBDA/lambda.py
@@ -0,0 +1,1 @@
+//  the anonymous functions are declared by using lambda keyword. However, Lambda functions can accept any number of arguments, but they can return only one value in the form of expression.
