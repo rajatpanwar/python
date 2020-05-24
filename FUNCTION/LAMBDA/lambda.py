ab9a86515054452bea87928c6c1d718e4e5b7c04
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-24 21:41:47 +0530

    the basic example of  lambda function

--- FUNCTION/LAMBDA/lambda.py
+++ FUNCTION/LAMBDA/lambda.py
@@ -1,1 +1,6 @@
 //  the anonymous functions are declared by using lambda keyword. However, Lambda functions can accept any number of arguments, but they can return only one value in the form of expression.
+
+
+<-----------example1--------------------->
+x=lambda a:a+10;
+print("sum is",x(30))  //output is --  40
