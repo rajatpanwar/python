17b2898b8a21f48209aa900d3e957318280b36e2
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-24 22:19:32 +0530

    what is file handling in python

---
+++ FILE HANDLING/file.py
@@ -0,0 +1,5 @@
+// Till now, we were taking the input from the console and writing it back to the console to interact with the user.
+
+Sometimes, it is not enough to only display the data on the console. The data to be displayed may be very large, and only a limited amount of data can be displayed on the console, and since the memory is volatile, it is impossible to recover the programmatically generated data again and again.
+
+However, if we need to do so, we may store it onto the local file system which is volatile and can be accessed every time. Here, comes the need of file handling.
