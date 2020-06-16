9d400984fe49542160f8299c6bd1da949561657e
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-16 19:03:00 +0530

    correct some error

--- HACKER_RANK/percentage.py
+++ HACKER_RANK/percentage.py
@@ -1,11 +1,11 @@
-n = int(input())                        //take input from users
-
-student_marks = {}                 //make a array
-
-for _ in range(n):
-    name, *line = input().split()          //The split() method splits a string into a list
-    scores = list(map(float, line))
-    student_marks[name] = scores
-query_name = input()
-
-print("{0:.2f}".format(round(sum(student_marks[query_name]) / len(student_marks[query_name]), 2)))
+def avg(lst):
+    return (lst[0]+lst[1]+lst[2])/3
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()            //The split() method splits a string into a list.
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    print(f'{avg(student_marks[query_name]):.2f}'
