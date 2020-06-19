be569c87bd8a5117dee93ddf8b871620129e9567
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-19 18:00:55 +0530

    after commit

--- HACKER_RANK/Mutations.py
+++ HACKER_RANK/Mutations.py
@@ -1,10 +1,10 @@
-def mutate_string(string, position, character):
+def mutate_string(string, position, character):    //create a function
     list2=list(string)
     list2[position]=character      
     return ''.join(list2)
 
-
-if __name__ == '__main__':
+  
+if __name__ == '__main__':     //main functiom
     s = input()
     i, c = input().split()
     s_new = mutate_string(s, int(i), c)
