48b12ffc555f8a50043fb78e43ae84636c51969b
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-19 17:50:10 +0530

    merge the tool

---
+++ HACKER_RANK/tool.py
@@ -0,0 +1,16 @@
+import textwrap
+from collections import OrderedDict
+def merge_the_tools(string, k):
+    list2=[]
+#devides string into n equal parts of size k
+    list1 = textwrap.wrap(string, k)
+
+    for i in list1:
+#removes duplicate characters from string and append it to list 2
+        list2.append("".join(OrderedDict.fromkeys(i)))
+
+    for i in list2:
+        print(i)
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
