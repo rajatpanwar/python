c6a41ecfaeebdba3d827c5cbb523200bb294e9f6
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-21 17:48:14 +0530

    collections.namedtupple.py create

---
+++ HACKER_RANK/nametupple.py
@@ -0,0 +1,6 @@
+from collections import namedtuple
+
+(n, categories) = (int(input()), input().split())
+Grade = namedtuple('Grade', categories)
+marks = [int(Grade._make(input().split()).MARKS) for _ in range(n)]
+print((sum(marks) / len(marks)))
