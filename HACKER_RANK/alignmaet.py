62fb1f94b255d2819a918d4d2fdae1a25b886efb
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-01 15:21:09 +0530

    add some comment

--- HACKER_RANK/alignmaet.py
+++ HACKER_RANK/alignmaet.py
@@ -7,6 +7,7 @@ c = 'H'
 for i in range(thickness):
     print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
 
+
 #Top Pillars
 for i in range(thickness+1):
     print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
