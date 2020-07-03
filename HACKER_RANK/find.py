676b0c9d41201b84c8ae4b6b1d76f75b98574eeb
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-03 16:21:09 +0530

    after correct some error

--- HACKER_RANK/find.py
+++ HACKER_RANK/find.py
@@ -4,7 +4,7 @@ def count_substring(string, sub_string):
     return(sum([1 for i in range(0, len(string) - len(sub_string) + 1) if (string[i:(len      (sub_string)+i)] == sub_string)]))
 
 if __name__ == '__main__':
-    string = input().strip()
+     string = input().strip()
     sub_string = input().strip()
     
     count = count_substring(string, sub_string)
