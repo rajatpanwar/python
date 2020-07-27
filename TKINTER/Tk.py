c0ae7fd80c5e52112c62e6ffe95476b8accab51e
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-27 22:02:27 +0530

    create the desktop application

---
+++ TKINTER/Tk.py
@@ -0,0 +1,8 @@
+from tkinter import *
+top = Tk()   #creating the application main window
+top.mainloop()   #Entering the event main loop
+
+
+
+
+#Python provides the standard library Tkinter for creating the graphical user interface for desktop based applications.
