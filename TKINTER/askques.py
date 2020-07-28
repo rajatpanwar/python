87b3be9985bbea53b21173247df1d2d6a6d81e5c
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-28 23:33:52 +0530

    after comment

--- TKINTER/askques.py
+++ TKINTER/askques.py
@@ -4,5 +4,5 @@ from tkinter import messagebox
   
 top = Tk()  
 top.geometry("100x100")  
-messagebox.askquestion("Confirm","Are you sure?")    
+messagebox.askquestion("Confirm","Are you sure?")    #this method is used for ask the question to the user which can be answered yes or no 
 top.mainloop()  
