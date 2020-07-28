750e908267516006bd6e861087e42b0ef8575c68
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-28 23:33:26 +0530

    use askquestion.py

---
+++ TKINTER/askques.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python3  
+from tkinter import *  
+from tkinter import messagebox  
+  
+top = Tk()  
+top.geometry("100x100")  
+messagebox.askquestion("Confirm","Are you sure?")    
+top.mainloop()  
