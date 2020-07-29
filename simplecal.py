7b14d07a2f904a49e4372350454a1e4540ecc73a
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-29 17:08:59 +0530

    after giving space

--- simplecal.py
+++ simplecal.py
@@ -1,6 +1,7 @@
 print(2**.5)             */output is 1.4142135623730951
 
 r=round(2**.5, 4)
+
 print(r)               */ output is 1.4142   because of round function if we want to calculate 4th digit after point then write the round() 
                           */function 
 
