9fdeba91511f49c65dece49111cec54e20b04fb5
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-08-02 02:06:48 +0530

    adding 1 to - button

--- TKINTER/Calculator_update.py
+++ TKINTER/Calculator_update.py
@@ -43,5 +43,22 @@ if __name__ == "__main__":     #main program
     entry.focus()
     
     
+#create button and adding into the fream
+
+btn7 = ttk.Button(top, text = '7' , width = 5 ,  command = lambda : press(7) )
+
+btn7.grid(row = 1 , column = 0 ,ipady = 4 , ipadx = 2)    
+    
+btn8 = ttk.Button(top, text = '8' , width = 5 ,  command = lambda : press(8))
+btn8.grid(row = 1 , column = 1 ,ipady = 4, ipadx = 2)
+
+btn9 = ttk.Button(top, text = '9' , width = 5 ,  command = lambda : press(9) )
+btn9.grid(row = 1 , column = 2,ipady = 4, ipadx = 2)
+
+btnmines = ttk.Button(top, text = '-' , width = 8 ,  command = lambda : press('-') )
+btnmines.grid(row = 1 , column = 3 , ipady = 3, ipadx = 2)
+
+    
+    
     
 
