e3818915b9f4d9b41c2d412afc43f5daf5273ac1
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-08-09 15:24:46 +0530

    after ccomment

--- HACKER_RANK/leap.py
+++ HACKER_RANK/leap.py
@@ -1,7 +1,7 @@
 def is_leap(n):             //define the function
     if n % 400 == 0:
         return True
-    if n % 100 == 0:
+    if n % 100 == 0:              #condition for leap year
         return False
     if n % 4 == 0:
         return True
